14bce327e26f3e7eb7c0613816e768eb96546645
Ernie Ripley <efripley@gmail.com>
2017-11-01 10:54:45 -0600

    Reorganized js into classes, beginning implementation of pongBalls on canvas

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,59 +1,123 @@
-//Initial state of the game... not logged in
-function initGame(){
-  //set up event listeners
-  uiLoginButton.onclick = logInUser;
-  uiCloseBtn.onclick = logOutUser;
-  uiLeaderboardBtn.onclick = toggleLeaderboard;
-  window.resize = resizeGame;
+//App class to control the flow of the game and overall game components
+function App(){
+  //initialize public variables
+  size = 2000;
+  pongBalls = new Array();
 
-  //initialize game state
-  resizeGame();
-  uiUsername.innerHTML = username;
+  //Initial state of the game... not logged in
+  this.init = function(){
+    //initialize game state
+    ui.init(this);
+  }
+
+  //The game loop
+  loop = function(){
+    for(var a = 0; a < pongBalls.length; a++){
+      pongBalls[a].x += pongBalls[a].xDir;
+      pongBalls[a].y += pongBalls[a].yDir;
+      if(pongBalls[a].x >= 2000 || pongBalls[a].x < 0){
+        pongBalls[a].xDir *= -1;
+      }
+      if(pongBalls[a].y >= 2000 || pongBalls[a].y < 0){
+        pongBalls[a].yDir *= -1;
+      }
+    }
+  }
+
+  //Happens on login form submission
+  this.logInUser = function(){
+    ui.logInUser();
+    pongBalls.push({type:"normal", x:0, y:0, xDir:Math.sqrt(2), yDir:Math.sqrt(2)});
+    LOOP = setInterval(loop, 1000/60);
+  }
+
+  //Happens on close button click
+  this.logOutUser = function(){
+    ui.logOutUser();
+    clearInterval(LOOP);
+  }
+
+  //Toggles Popup showing framerate keycodes and other usefull info
+  this.toggleDebugMode = function(){
+    debugMode = !debugMode;
+  }
 }
 
-//In the event the user resizes browser
-function resizeGame(){
+//User Interface Class to controll the user interface elements
+function Ui(){
+  //initialize private ui variables
+  loginForm = document.getElementById("login-cmp");
+  loginName = document.getElementById("nickname-input");
+  loginBtn = document.getElementById("nickname-button");
+  closeBtn = document.getElementById("close-cmp");
+  username = document.getElementById("username");
+  canvas = document.getElementById("canvas");
+  leaderboard = document.getElementById("leaderboard");
+  leaderboardBtn = document.getElementById("leaderboard-btn");
+
   screenWidth = window.innerWidth;
   screenHeight = window.innerHeight;
-  uiCanvas.setAttribute("width", screenWidth.toString());
-  uiCanvas.setAttribute("height", screenHeight.toString());
+
+  user = "Not Logged In";
+
+
+  //set up ui event listeners
+  this.init = function(app){
+    loginBtn.onclick = app.logInUser;
+    closeBtn.onclick = app.logOutUser;
+    leaderboardBtn.onclick = toggleLeaderboard;
+    window.resize = resize;
+    username.innerHTML = user;
+    window.onkeypress = processAppInput;
+  }
+
+  //In the event the user resizes browser
+  resize = function(){
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    uiCanvas.setAttribute("width", screenWidth.toString());
+    uiCanvas.setAttribute("height", screenHeight.toString());
+  }
+
+  //Toggles a few ui components and updates info
+  this.logOutUser = function(){
+    loginForm.classList.toggle("visible");
+    user = "Not Logged In";
+    username.innerHTML = user;
+    closeBtn.classList.toggle('visible');
+  }
+
+  //Toggles a few ui components and updates info
+  this.logInUser = function(){
+    loginForm.classList.toggle("visible");
+    user = loginName.value;
+    username.innerHTML = user;
+    closeBtn.classList.toggle('visible');
+  }
+
+  processAppInput = function(event){
+    //input if user is logged in
+    if(!loginForm.classList.contains("visible")){
+      console.log(event.keyCode);
+      if(event.keyCode == 113)//Q
+        app.logOutUser();
+      else if(event.keyCode == 63)//?
+        app.toggleDebugMode();
+    }
+    //input if user is not logged in
+    else{
+      if(event.keyCode == 13)//ENTER
+        app.logInUser();
+    }
+  }
+
+  toggleLeaderboard = function(){
+    leaderboard.classList.toggle('visible');
+  }
 }
 
-//Happens on close button click
-//Toggles a few ui components and triggers user-server disconnect intereaction
-function logOutUser(){
-  uiLoginForm.classList.toggle("visible");
-  username = "Not Logged In";
-  uiUsername.innerHTML = username;
-  uiCloseBtn.classList.toggle('visible');
-}
+app = new App();
+ui = new Ui();
 
-//Happens on login form submission
-//Toggles a few ui components and triggers user-server initial intereaction
-function logInUser(){
-  uiLoginForm.classList.toggle("visible");
-  username = uiLoginName.value;
-  uiUsername.innerHTML = username;
-  uiCloseBtn.classList.toggle('visible');
-}
-
-function toggleLeaderboard(){
-  uiLeaderboard.classList.toggle('visible');
-}
-
-//initialize variables
-screenWidth = window.innerWidth;
-screenHeight = window.innerHeight;
-username = "Not Logged In";
-
-//initialize ui variables
-uiLoginForm = document.getElementById("login-cmp");
-uiLoginName = document.getElementById("nickname-input");
-uiLoginButton = document.getElementById("nickname-button");
-uiCloseBtn = document.getElementById("close-cmp");
-uiUsername = document.getElementById("username");
-uiCanvas = document.getElementById("canvas");
-uiLeaderboard = document.getElementById("leaderboard");
-uiLeaderboardBtn = document.getElementById("leaderboard-btn");
-
-initGame();
+app.init();
+ui.init(app);
